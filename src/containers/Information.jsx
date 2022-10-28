6596d9cd254ee479a88433d4932b636d694352cc
alejo1garciasosa@gmail.com <alejandrogh7>
2022-10-28 11:51:09 -0500

    setup all

---
+++ src/containers/Information.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/components/Information.css';
+
+const Information = () => {
+  return (
+    <div>Information</div>
+  );
+};
+
+export default Information;
---
+++ src/containers/Payment.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/components/Payment.css';
+
+const Payment = () => {
+  return (
+    <div>Payment</div>
+  );
+};
+
+export default Payment;
